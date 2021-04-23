5dc29ae83db4ff955247f496fab441aa57ca54a3
dah00 <61763829+dah00@users.noreply.github.com>
2021-04-23 12:20:46 -0700

    Gains project
    Setting up repository

---
+++ Project/Gains/src/application/Controller.java
@@ -0,0 +1,157 @@
+package application;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.sql.Date;
+import java.net.URL;
+import java.util.Date;
+import java.util.ResourceBundle;
+
+/**
+ * 
+ * @author Anthony Tran, Andrilalaina Obeda Velonjatovo, Benjamin Seang, Nathanael Garza			
+ *
+ * Controller class for the program
+ *
+ */
+
+
+public class Controller implements Initializable {
+    @FXML
+    private TextField exerciseNameLabel;
+    @FXML
+    private TextField numberOfSetsLabel;
+    @FXML
+    private TextField avgSetDurationLabel;
+    @FXML
+    private TextField durationLabel;
+    @FXML
+    private TextField numberOfRepsLabel;
+    @FXML
+    private TextField amountOfWeightLabel;
+    @FXML
+    private TextField difficultyLevelLabel;
+    @FXML
+    private DatePicker myDatePicker;
+    @FXML
+    private TextArea lastWorkoutTextArea;
+    
+
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+    private WorkoutController workout = new WorkoutController();
+    
+    @Override
+    public void initialize(URL arg0, ResourceBundle arg1) {
+
+    	displayLastWorkout();
+
+	}
+
+    //display last workout
+    public void displayLastWorkout()
+    {
+    	if(lastWorkoutTextArea != null)
+    	{
+    		lastWorkoutTextArea.setText("Hello");
+    	}
+    }
+
+    // Switch to Homepage 
+    public void switchToHomepage(ActionEvent event) throws IOException
+    {
+    	switchToPage(event, "Homepage.fxml");
+    }
+    
+    // Switch to Workout Type Select 
+    public void switchToWorkoutTypeSelect(ActionEvent event) throws IOException
+    {
+    	switchToPage(event, "WorkoutTypeSelect.fxml");
+    }
+    
+   // Switch to AddWeightTraining 
+    public void switchAddToWeightTraining(ActionEvent event) throws IOException
+    {
+    	switchToPage(event, "AddWeightTraining.fxml");
+    }
+    
+    // Switch to Cardio 
+    public void switchToAddCardio(ActionEvent event) throws IOException
+    {
+    	switchToPage(event, "AddCardio.fxml");
+    }
+    
+    public void saveWeightExercise(ActionEvent e) {
+
+    }
+    
+    public void saveCardioExercise(ActionEvent e) {
+    	
+    }
+
+    // Grab and save user's input from Add Weight Training page
+    public void submitAndSaveWeightTraining(ActionEvent e)
+    {
+        String exerciseName = exerciseNameLabel.getText();
+        String numberOfReps = numberOfRepsLabel.getText();
+        String numberOfSets = numberOfSetsLabel.getText();
+        String difficultyLevel = difficultyLevelLabel.getText();
+        String avgSetDuration = avgSetDurationLabel.getText();
+        String amountOfWeight = amountOfWeightLabel.getText();
+        String date = myDatePicker.getValue().toString(); // LocalDate or String
+        int sets = Integer.parseInt(numberOfSets);
+        int reps = Integer.parseInt(numberOfReps);
+        int weight = Integer.parseInt(amountOfWeight);
+        Date dateDate = new Date(2021-04-22); // Need it to be SQL Date
+        WeightTraining entry = new WeightTraining("nate", exerciseName, difficultyLevel, avgSetDuration, dateDate, weight, sets, reps);
+        workout.addWeightsExercise(entry);
+        // // Build a string that is being saved to the save file
+        // String data =  	date
+        // 				+ " " + exerciseName
+        // 				+ " " + numberOfSets
+        // 				+ " " + numberOfReps
+        // 				+ " " + avgSetDuration
+        // 				+ " " + difficultyLevel;
+        // // Save user's input
+        // utility.saveWorkoutToFile(data);
+    }
+    
+    // Grab user's input from Add Cardio page
+    public void submitAndSaveCardio(ActionEvent e)
+    {
+        String exerciseName = exerciseNameLabel.getText();
+        String difficultyLevel = difficultyLevelLabel.getText();
+        String duration = durationLabel.getText();
+        String date = myDatePicker.getValue().toString();
+
+        // Build a string that is being saved to the save file
+        String data =  	date
+        				+ " " + exerciseName
+        				+ " " + duration
+        				+ " " + difficultyLevel;
+        
+        // Save user's input to a txt file
+        utility.saveWorkoutToFile(data);
+    }
+    
+    private void switchToPage(ActionEvent event, String targetPage) throws IOException
+    {
+        root = FXMLLoader.load(getClass().getResource(targetPage));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+}
---
+++ Project/Gains/src/application/WorkoutController.java
@@ -0,0 +1,63 @@
+package application;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class WorkoutController {
+	private String host = "cs151-gains-db.cjsjim1qtdlq.us-west-2.rds.amazonaws.com/";
+	private String dbName = "gains";
+	private String dbUsername = "root";
+	private String dbPassword = "Leanahtan523509";
+	private String user = "nate";
+	private MySQLDatabase database;
+	
+	WorkoutController(){
+		database = new MySQLDatabase(host, dbName, dbUsername, dbPassword);
+	}
+
+	public ResultSet getWorkouts(String dbTable) {
+		System.out.println("Fetching" + dbTable +" workouts...");
+		String sql = "SELECT * FROM " + dbTable + " WHERE username = 'user' ORDER BY workoutDate DESC";
+		return database.queryStatement(sql);
+	}
+	
+	public ResultSet getRecentWorkout(String dbTable) {
+		System.out.println("Fetching most recent workouts...");
+		String sql = "SELECT * FROM " + dbTable + " WHERE username = 'user' ORDER BY workoutDate DESC limit 2";
+		return database.queryStatement(sql);
+	}
+	
+	public void addCardioExercise(CardioTraining exercise) {
+		System.out.println("Adding cardio exercise to database...");
+		String sql = "INSERT INTO cardio (username, workoutName, difficulty, duration workoutDate) VALUES (?,?,?,?,?)";
+		try (PreparedStatement statement = database.connectDatabase().prepareStatement(sql)) {
+			statement.setString(1, exercise.getUsername());
+			statement.setString(2, exercise.getWorkoutName());
+			statement.setString(3, exercise.getDifficulty());
+			statement.setString(4, exercise.getDuration());
+			statement.setDate(5, exercise.getWorkoutDate());
+			statement.executeUpdate();
+		} catch (SQLException e) {
+			database.sqlExceptionError(e);
+		}
+	}
+	
+	public void addWeightsExercise(WeightTraining exercise) {
+		System.out.println("Adding weights exercise to database...");
+		String sql = "INSERT INTO workout (username, workoutName, difficulty, duration, workoutDate, weight, sets, reps) VALUES (?,?,?,?,?,?,?,?)";
+		try (PreparedStatement statement = database.connectDatabase().prepareStatement(sql)) {
+			statement.setString(1, exercise.getUsername());
+			statement.setString(2, exercise.getWorkoutName());
+			statement.setString(3, exercise.getDifficulty());
+			statement.setString(4, exercise.getDuration());
+			statement.setDate(5, exercise.getWorkoutDate());
+			statement.setInt(6, exercise.getWeight());
+			statement.setInt(7, exercise.getSets());
+			statement.setInt(8, exercise.getReps());
+			statement.executeUpdate();
+		} catch (SQLException e) {
+			database.sqlExceptionError(e);
+		}
+	}
+}
